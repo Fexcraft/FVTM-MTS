2ef4604b5256706cce854e7ecf11654a97657692
Ferdinand <fercalo96@yahoo.de>
2021-05-02 23:24:26 +0200

    Update MTSCompat.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
@@ -13,7 +13,7 @@ public class MTSCompat {
 	
     public static final String MODID = "fvtm_mts";
     public static final String NAME = "FVTM MTS Compat";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     //public static Item ITEM = new Item();
 
     @EventHandler
