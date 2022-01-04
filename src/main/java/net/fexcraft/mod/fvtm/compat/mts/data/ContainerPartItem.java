1cb7d7fe7118a62336cd43fe7b68f0edd149414b
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-04 23:02:34 +0100

    Update ContainerPartItem.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPartItem.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPartItem.java
@@ -5,6 +5,7 @@ import minecrafttransportsimulator.entities.instances.APart;
 import minecrafttransportsimulator.items.components.AItemPart;
 import minecrafttransportsimulator.jsondefs.JSONPartDefinition;
 import minecrafttransportsimulator.mcinterface.WrapperNBT;
+import minecrafttransportsimulator.mcinterface.WrapperPlayer;
 import net.fexcraft.lib.mc.registry.FCLRegistry;
 import net.fexcraft.mod.fvtm.compat.mts.MTSCompat;
 import net.minecraft.item.ItemStack;
@@ -14,18 +15,23 @@ public class ContainerPartItem extends AItemPart {
 	private boolean single;
 	
 	public ContainerPartItem(ContainerJsonPart conpart, boolean single){
-		super(conpart, "", MTSCompat.MODID);
+		super(conpart, null, MTSCompat.MODID);
 		this.single = single;
 	}
 
 	@Override
-	public APart createPart(AEntityE_Multipart<?> entity, JSONPartDefinition packVehicleDef, WrapperNBT partData, APart parentPart){
-		return new ContainerPart(entity, packVehicleDef, partData, parentPart);
+	public APart createPart(AEntityE_Multipart<?> entity, WrapperPlayer placingPlayer, JSONPartDefinition packVehicleDef, WrapperNBT partData, APart parentPart) {
+		return new ContainerPart(entity, placingPlayer, packVehicleDef, partData, parentPart);
 	}
 	
 	@Override
 	public ItemStack getNewStack(){
 		return new ItemStack(FCLRegistry.getItem("fvtm_mts:container_" + (single ? "single" : "double")));
+	}
+	
+	@Override
+	public boolean autoGenerate(){
+		return false;
 	}
 
 }
