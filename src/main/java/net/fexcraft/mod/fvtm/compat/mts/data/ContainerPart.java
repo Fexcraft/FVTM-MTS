98949185957d60fade40bfe477f8331d0efb1fef
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-04 23:02:32 +0100

    Update ContainerPart.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPart.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPart.java
@@ -4,11 +4,12 @@ import minecrafttransportsimulator.entities.components.AEntityE_Multipart;
 import minecrafttransportsimulator.entities.instances.APart;
 import minecrafttransportsimulator.jsondefs.JSONPartDefinition;
 import minecrafttransportsimulator.mcinterface.WrapperNBT;
+import minecrafttransportsimulator.mcinterface.WrapperPlayer;
 
 public class ContainerPart extends APart {
 
-	public ContainerPart(AEntityE_Multipart<?> entityOn, JSONPartDefinition placementDefinition, WrapperNBT data, APart parentPart){
-		super(entityOn, placementDefinition, data, parentPart);
+	public ContainerPart(AEntityE_Multipart<?> entityOn, WrapperPlayer player, JSONPartDefinition placementDefinition, WrapperNBT data, APart parentPart){
+		super(entityOn, player, placementDefinition, data, parentPart);
 	}
 
 }
