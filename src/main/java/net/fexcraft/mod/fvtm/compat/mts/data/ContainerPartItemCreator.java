f876fc5899b66e3e53387e562e1dc36314adbe78
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-05 20:52:36 +0100

    Update ContainerPartItemCreator.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPartItemCreator.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerPartItemCreator.java
@@ -3,14 +3,11 @@ package net.fexcraft.mod.fvtm.compat.mts.data;
 import minecrafttransportsimulator.items.components.AItemPart;
 import minecrafttransportsimulator.items.components.AItemPart.AItemPartCreator;
 import minecrafttransportsimulator.jsondefs.JSONPart;
+import net.fexcraft.mod.fvtm.compat.mts.MTSCompat;
 
 public class ContainerPartItemCreator extends AItemPartCreator {
 
-	private ContainerPartItem ipc;
-
-	public ContainerPartItemCreator(ContainerPartItem con){
-		this.ipc = con;
-	}
+	public ContainerPartItemCreator(){}
 
 	@Override
 	public boolean isCreatorValid(JSONPart definition){
@@ -19,7 +16,7 @@ public class ContainerPartItemCreator extends AItemPartCreator {
 
 	@Override
 	public AItemPart createItem(JSONPart definition, String subName, String sourcePackID){
-		return ipc;
+		return definition.systemName.contains("single") ? MTSCompat.CON_ITEM_SINGLE : MTSCompat.CON_ITEM_DOUBLE;
 	}
 	
 }
