ae246d94fca71893416a8751841f50c586e3f7de
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-05 20:52:25 +0100

    Update ContainerJsonPart.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerJsonPart.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerJsonPart.java
@@ -10,11 +10,13 @@ public class ContainerJsonPart extends JSONPart {
 		generic.type = "interactable_container_" + type;
 		generic.customType = null;
 		generic.width = generic.height = 1;
+		interactable = new JSONPartInteractable();
+		interactable.interactionType = InteractableComponentType.CRATE;
 		general = new General();
 		general.description = "FVTM Compat Container Slot Provider\nSize: " + (type.equals("single") ? 6 : 12);
 		general.name = (type.equals("single") ? "Single" : "Double") + " Container";
 		general.stackSize = 1;
-		classification = ItemClassification.ITEM;
+		classification = ItemClassification.PART;
 		systemName = "container_holder_" + type;
 		packID = "fvtm_mts";
 	}
