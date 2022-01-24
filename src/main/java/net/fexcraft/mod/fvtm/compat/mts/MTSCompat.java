8316236b65bad178b67294fd5184254916c8b984
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-24 21:00:42 +0100

    Update MTSCompat.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
@@ -7,13 +7,10 @@ import net.fexcraft.mod.fvtm.compat.mts.data.ContainerJsonPart;
 import net.fexcraft.mod.fvtm.compat.mts.data.ContainerPartItem;
 import net.fexcraft.mod.fvtm.compat.mts.data.ContainerPartItemCreator;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(modid = MTSCompat.MODID, name = MTSCompat.NAME, version = MTSCompat.VERSION, dependencies = "required-after:fcl;required-after:fvtm")
 public class MTSCompat {
@@ -39,12 +36,12 @@ public class MTSCompat {
     	
     	
     	// old compat code bellow
-        MinecraftForge.EVENT_BUS.register(new CompatEvents());
-		EntityRegistry.registerModEntity(new ResourceLocation("fvtm_mts:tracker"), Tracker.class, "fvtm_mts.tracker", 0, this, 512, 1, false);
-        if(event.getSide().isClient()){
-        	MinecraftForge.EVENT_BUS.register(new net.fexcraft.mod.fvtm.compat.mts.RenderEvents());
-			net.minecraftforge.fml.client.registry.RenderingRegistry.registerEntityRenderingHandler(Tracker.class, RenderTracker::new);
-        }
+        //MinecraftForge.EVENT_BUS.register(new CompatEvents());
+		//EntityRegistry.registerModEntity(new ResourceLocation("fvtm_mts:tracker"), Tracker.class, "fvtm_mts.tracker", 0, this, 512, 1, false);
+        //if(event.getSide().isClient()){
+        //	MinecraftForge.EVENT_BUS.register(new net.fexcraft.mod.fvtm.compat.mts.RenderEvents());
+		//	net.minecraftforge.fml.client.registry.RenderingRegistry.registerEntityRenderingHandler(Tracker.class, RenderTracker::new);
+        //}
     }
 
     @EventHandler
