8f7837df48f0bbeb7b2cbddc7d8e25683effb5e0
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-24 21:00:35 +0100

    Update ContainerItem.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerItem.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/data/ContainerItem.java
@@ -1,13 +1,23 @@
 package net.fexcraft.mod.fvtm.compat.mts.data;
 
-import minecrafttransportsimulator.mcinterface.BuilderItem;
+import minecrafttransportsimulator.items.components.AItemBase;
+import minecrafttransportsimulator.mcinterface.IBuilderItemInterface;
 import net.fexcraft.mod.fvtm.InternalAddon;
+import net.minecraft.item.Item;
 
-public class ContainerItem extends BuilderItem {
+public class ContainerItem extends Item implements IBuilderItemInterface {
+	
+	private AItemBase base;
 
 	public ContainerItem(ContainerPartItem conpart){
-		super(conpart);
+		super();
+		this.base = conpart;
 		setCreativeTab(InternalAddon.INSTANCE.getDefaultCreativeTab());
+	}
+
+	@Override
+	public AItemBase getItem(){
+		return base;
 	}
 
 }
