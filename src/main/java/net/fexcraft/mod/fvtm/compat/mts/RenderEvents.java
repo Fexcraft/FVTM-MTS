9f4462e8999677e10f4d20666239db9f78170b8a
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-24 23:43:14 +0100

    Update RenderEvents.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/RenderEvents.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/RenderEvents.java
@@ -5,15 +5,16 @@ import org.lwjgl.opengl.GL11;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class RenderEvents {
 	
 	private static Entity camera;
 	private static double x, y, z;
 	
-    //@SubscribeEvent
-    public void renderRails(RenderWorldLastEvent event){
-        float ticks = Minecraft.getMinecraft().getRenderPartialTicks();
+    @SubscribeEvent
+    public void render(RenderWorldLastEvent event){
+        /*float ticks = Minecraft.getMinecraft().getRenderPartialTicks();
         Entity camera = Minecraft.getMinecraft().getRenderViewEntity();
         x = camera.lastTickPosX + (camera.posX - camera.lastTickPosX) * ticks;
         y = camera.lastTickPosY + (camera.posY - camera.lastTickPosY) * ticks;
@@ -23,7 +24,7 @@ public class RenderEvents {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glTranslated(-x, -y, -z);
         //
-		GL11.glPopMatrix();
+		GL11.glPopMatrix();*/
     }
     
     public static void translate(float ticks){
