f53dc9efe7dc2bdd487d1cd07bdfcd24c8b20013
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-27 22:24:05 +0100

    Update MTSCompat.java

--- src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
+++ src/main/java/net/fexcraft/mod/fvtm/compat/mts/MTSCompat.java
@@ -7,11 +7,13 @@ import net.fexcraft.mod.fvtm.compat.mts.data.ContainerJsonPart;
 import net.fexcraft.mod.fvtm.compat.mts.data.ContainerPartItem;
 import net.fexcraft.mod.fvtm.compat.mts.data.ContainerPartItemCreator;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(modid = MTSCompat.MODID, name = MTSCompat.NAME, version = MTSCompat.VERSION, dependencies = "required-after:fcl;required-after:fvtm")
 public class MTSCompat {
@@ -34,15 +36,11 @@ public class MTSCompat {
     	PackParserSystem.registerItem(CON_PART_DOUBLE);
     	FCLRegistry.newAutoRegistry(MODID).addItem("container_single", CON_SINGLE = new ContainerItem(CON_ITEM_SINGLE), 0, null);
     	FCLRegistry.getAutoRegistry(MODID).addItem("container_double", CON_DOUBLE = new ContainerItem(CON_ITEM_DOUBLE), 0, null);
-    	
-    	
-    	// old compat code bellow
         MinecraftForge.EVENT_BUS.register(new CompatEvents());
-		//EntityRegistry.registerModEntity(new ResourceLocation("fvtm_mts:tracker"), Tracker.class, "fvtm_mts.tracker", 0, this, 512, 1, false);
-        //if(event.getSide().isClient()){
-        //	MinecraftForge.EVENT_BUS.register(new net.fexcraft.mod.fvtm.compat.mts.RenderEvents());
-		//	net.minecraftforge.fml.client.registry.RenderingRegistry.registerEntityRenderingHandler(Tracker.class, RenderTracker::new);
-        //}
+		EntityRegistry.registerModEntity(new ResourceLocation("fvtm_mts:tracker"), Tracker.class, "fvtm_mts.tracker", 0, this, 512, 1, false);
+        if(event.getSide().isClient()){
+			net.minecraftforge.fml.client.registry.RenderingRegistry.registerEntityRenderingHandler(Tracker.class, RenderTracker::new);
+        }
     }
 
     @EventHandler
